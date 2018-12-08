8ffc642df116f456a8d940a1a7b2595eb13d30e0
TheSteelGuy <collotuos0@gmail.com>
2018-12-08 13:12:31 +0300

    initial commit

---
+++ output81/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>WORK IN PROGRESS>>></h1>
+            </div>
+        );
+    }
+}
+
+export default App;
